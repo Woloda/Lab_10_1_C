4665ddccfc4196ddd811abf8e58ded9d511a9f6c
Woloda <dorozovecvoloda@gmail.com>
2020-12-13 19:02:22 +0200

    Lab__10_1_C

---
+++ Lab_10.1(C)/Lab_10.1(C).cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+bool perevirka(char* fname);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char fname[51];
+	cout << "Введіть назву фізичного файлу: ";
+	cin.getline(fname, sizeof(fname));
+	cout << endl;
+
+	bool b = perevirka(fname);
+
+	if (b)
+		cout << "Так є серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+	else
+		cout << "Ні не має серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+
+	return 0;
+}
+
+bool perevirka(char* fname)
+{
+	FILE* perev_f;
+
+	if ((perev_f = fopen(fname, "r")) == NULL)
+	{
+		cerr << "Помилка відкриття фізичного файлу '" << fname << "'" << endl;
+		exit(1);
+	}
+
+	const int size = 41;
+	char lit_r[size];
+
+	while (!feof(perev_f))
+	{
+		fgets(lit_r, size - 1, perev_f);
+		cout << "Літерний рядок зчитаний з фізичного файлу: ";
+		cout << lit_r << endl << endl;
+	}
+
+	for (int i = 0; i < strlen(lit_r); i++)
+	{
+		if ((lit_r[i] == 'Q' && lit_r[i + 1] == 'S') || (lit_r[i] == 'S' && lit_r[i + 1] == 'Q'))
+			return true;
+	}
+	return false;
+}
---
+++ UnitTest(Lab_10.1(C))/UnitTest(Lab_10.1(C)).cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_10.1(C)/Lab_10.1(C).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab101C
+{
+	TEST_CLASS(UnitTestLab101C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[51] = "test.txt";
+
+			bool b = perevirka(fname);
+
+			Assert::AreEqual(b, true);
+		}
+	};
+}
